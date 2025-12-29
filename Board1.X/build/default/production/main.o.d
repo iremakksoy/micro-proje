73e948cad6cafbc4893d0e0ac1a290b55fc7454a
gizemkt <gizemkkuvvett@gmail.com>
2025-12-29 21:54:31 +0300

    Delete Board1.X directory

--- Board1.X/build/default/production/main.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/main.o:  \
-	C:/Users/deniz/OneDrive/Desktop/Ders/Micro/Board1.X/main.asm  \
-	C:/Users/deniz/OneDrive/Desktop/Ders/Micro/Board1.X/common.inc  \
-	C:/Users/deniz/OneDrive/Desktop/Ders/Micro/Board1.X/tempctrl.asm  \
-	C:/Users/deniz/OneDrive/Desktop/Ders/Micro/Board1.X/keypad.asm  \
-	C:/Users/deniz/OneDrive/Desktop/Ders/Micro/Board1.X/display.asm  \
-	C:/Users/deniz/OneDrive/Desktop/Ders/Micro/Board1.X/uart.asm  \
-
